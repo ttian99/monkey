cf22bff254cc8c81fbed46a64e65f4ac558cccb1
ttian99 <176553611@qq.com>
2016-09-18 19:02:52 +0800

    更新数据获取逻辑，更新数据结构

--- test/xwf-data-tool/getShortdata.js
+++ test/xwf-data-tool/getShortdata.js
@@ -4,10 +4,7 @@ var psql = require('./psql.js');
 var _ = require('lodash');
 var cfg = null;
 var async = require('async');
-// var arr = ['长源村', '福光村', '福林村', '塘朗村', '塘朗小学', '田寮村', '杨屋村'];
-// var words = arr[0];
-// var sqlStr = "select houseaddress from room where houseaddress like '" + words + "%'";
-
+var v2sObj = require('./lib/xwf-v2s-data.js');
 
 var isSearch = false;
 
@@ -19,9 +16,12 @@ function checkOutDir(dir) {
   }
 }
 
-// // 读取project.json配置文件
+// 读取project.json配置文件
 fsr('./project.json', function(err, data) {
-  if (err) console.log(err);
+  if (err){
+    console.log(err);
+    return;
+  } 
   cfg = data;
   // 初始化默认编码
   var initAreaCode = require('./lib/area-code-def.js');
@@ -30,75 +30,81 @@ fsr('./project.json', function(err, data) {
   checkOutDir(cfg.v2sDir);
   // 输出表格名数组
   var sheetArr = cfg.outArr;
-  // getData(sheetArr);
-  // _.map(sheetArr, function(item, id) {
-  //   main(item);
-  // });
-  main();
+
+  var idx = cfg.idx;
+  idx = addIdx(idx);
+  
+  startApp(idx);
 });
 
-function main(sheetName) {
-  console.log('--------- main ---------');
-  var words = "白芒村";
-  // var fullObj = {};
-  // var code = cfg
-  // var sqlStr = "select houseaddress from room where houseaddress like '" + words + "%'";
+
+function startApp(idx) {
+  console.log('-- startApp idx : ' + idx + ' --');
+  var words = getWords(idx);
+
+  if (!words) {
+    console.log('-- getWords error --');
+    return;
+  }
+  reqData(words, idx);
+}
+
+// 记录值+1
+function addIdx(idx) {
+  if (!idx) {
+    console.log('-- idx error!!! --');
+    idx = '402-00000';
+    return idx;
+  }
+  var str = idx + '';
+  var head = str.slice(0, 4);
+  var end = str.slice(4);
+  var num = parseInt(end);
+  num++;
+  console.log('head = ' + head + ' | end = ' +end + ' | num = ' + num);
+  var code = _.padStart(num + '', 5, '0');
+  var newIdx = head + code;
+  console.log('-- newIdx = ' + newIdx);
+  return newIdx;
+}
+
+// 获取关键字
+function getWords(idx) {
+  if (idx === '402-00941' ) {
+    idx = '404-00000';
+  } else if (idx === '404-00092') {
+    idx = '8010';
+  }
+  var key = _.findKey(v2sObj, {'idx': idx});
+  return key ? key : '';
+}
+
+// 请求数据库数据
+function reqData(words, idx) {
+  console.log('-- reqData words : ' + words + ' , idx : ' + idx + ' --');
+
   async.waterfall(
     [
       function(callback) {
         psqlReq(words, function(err, data) {
           // 记录原始数据
           var jsonStr = JSON.stringify(data);
-          var dst = cfg.v2sDir + "402-00001-oral" + cfg.extName;
+          var dst = cfg.v2sDir + idx + "-oral" + cfg.extName;
           outFile(jsonStr, dst, function() {
             callback(null, data);
           });
         });
       },
-      // function(oralArr, callback) {
-      //   getKeyArr(words, oralArr, function(err, data) {
-      //     var jsonStr = JSON.stringify(data);
-      //     var dst = cfg.v2sDir + "402-00001-key" + cfg.extName;
-      //     // 记录关键字数组
-      //     outFile(jsonStr, dst, function() {
-      //       var keyArr = data;
-      //       callback(null, oralArr, keyArr);
-      //     });
-      //   });
-      // },
-
-      // function(oralArr, keyArr, callback) {
-      //   var keyObj = {};
-      //   var keyLen = keyArr.length;
-      //   console.log('-- keyLen = ' + keyLen);
-      //   _.map(keyArr, function(item, id) {
-      //     getBuildingArr(item, oralArr, function(err, data){
-      //       keyObj[item] = data;
-      //     });
-
-      //     if (id === keyLen - 1) {
-      //       var jsonStr = JSON.stringify(keyObj);
-      //       var dst = cfg.v2sDir + '402-00001-' + words + '-buildArr' + cfg.extName;
-      //       outFile(jsonStr, dst, function() {
-      //         callback(null, oralArr, keyObj);
-      //         console.log('------ over1 ------');
-      //       });
-      //     };
-      //   });
-      // },
       function(oralArr, callback) {
-        console.log('---- getKBR -----');
-        // console.log('-- oralArr - ' + JSON.stringify(oralArr));
         keyMode(words, oralArr, function(err, data) {
-          console.log('------- getKBR over -----------');
           callback(err, data);
         })
       }
     ],
     function(err, result) {
-      console.log('------ waterfall over ------');
+      // console.log('------ waterfall over ------');
       var jsonStr = JSON.stringify(result);
-      var dst = cfg.v2sDir + "402-00001" + cfg.extName;
+      var dst = cfg.v2sDir + idx + cfg.extName;
       outFile(jsonStr, dst, function() {
         console.log('----- out file : ' + dst + ' ----');
       });
@@ -107,7 +113,7 @@ function main(sheetName) {
 
 // psql请求
 function psqlReq(words, cb) {
-  var sqlStr = "select * from room where houseaddress like '%" + words + "%'";
+  var sqlStr = "select * from room where code='" + words + "'";
   psql(sqlStr, cb);
   // psql(sqlStr, function(err, data) {
   //   var jsonStr = JSON.stringify(data);
@@ -128,7 +134,7 @@ function outFile(jsonStr, dst, cb) {
       cb && cb(err, data);
       return;
     }
-    console.log("-- file have writeFile at " + dst);
+    console.log("-- data file have writeFile at " + dst);
     cb && cb(null, data);
   });
 }
@@ -158,44 +164,6 @@ function getKeyArr(words, arr, cb) {
       // 数组去重
       tmp = _.uniq(tmp);
       cb(null, tmp);
-    }
-  });
-}
-
-
-function getBuildingArr(key, arr, cb) {
-  var tmp = [];
-  _.map(arr, function(item, id) {
-    var str = item['houseaddress'];
-    var reg = new RegExp(key, 'g');
-    if (!reg.test(str)) {
-      // console.log('no');
-    } else {
-      str = _.trim(str); // 去掉多余的空格
-      var strArr = str.split('');
-      var startPos = _.findIndex(strArr, function(item) {
-        var aa = /^[0-9]*$/.exec(item);
-        return aa;
-      });
-
-      var endPos = _.findLastIndex(strArr, function(item) {
-        var bb = /[^A-Za-z0-9]$/.exec(item);
-        return bb;
-      });
-
-      var mPos = strArr.length - endPos - 1;
-
-      var match = _.drop(strArr, startPos);
-      match = _.dropRight(match, mPos);
-      var goodStr = match.join("");
-      // tmp.push({name: goodStr});
-      tmp.push(goodStr);
-    }
-
-    if (id === arr.length - 1) {
-      // 数组去重
-      tmp = _.uniq(tmp);
-      cb && cb(null, tmp);
     }
   });
 }
@@ -306,7 +274,11 @@ function keyMode(words, arr, cb) {
       obj[key][building] = [];
     }
 
-    obj[key][building].push(room);
+    var code = _.trim(item.code);
+    var tmpObj = {};
+    tmpObj.room = room;
+    tmpObj.code = code;
+    obj[key][building].push(tmpObj);
 
     // 数组去重
     // if (obj[key][building]) {
@@ -321,6 +293,59 @@ function keyMode(words, arr, cb) {
       // 数组去重
       console.log('----- map over ------');
       cb(null, obj);
+    }
+  });
+}
+
+// 保存记录下的idx
+function saveIdx(idx) {
+  cfg.idx = idx;
+  var jsonStr = JSON.stringify(cfg);
+  // 读取project.json配置文件
+  fsw('./project.json', jsonStr, function(err, data) {
+    if (err){
+      console.log(err);
+    } 
+
+    console.log('-- saveIdx over => idx : ' + idx + '--');
+    startApp(idx);
+  });
+}
+
+
+function getBuildingArr(key, arr, cb) {
+  var tmp = [];
+  _.map(arr, function(item, id) {
+    var str = item['houseaddress'];
+    var reg = new RegExp(key, 'g');
+    if (!reg.test(str)) {
+      // console.log('no');
+    } else {
+      str = _.trim(str); // 去掉多余的空格
+      var strArr = str.split('');
+      var startPos = _.findIndex(strArr, function(item) {
+        var aa = /^[0-9]*$/.exec(item);
+        return aa;
+      });
+
+      var endPos = _.findLastIndex(strArr, function(item) {
+        var bb = /[^A-Za-z0-9]$/.exec(item);
+        return bb;
+      });
+
+      var mPos = strArr.length - endPos - 1;
+
+      var match = _.drop(strArr, startPos);
+      match = _.dropRight(match, mPos);
+      var goodStr = match.join("");
+      // tmp.push({name: goodStr});
+      tmp.push(goodStr);
+    }
+
+    if (id === arr.length - 1) {
+      // 数组去重
+      tmp = _.uniq(tmp);
+      cb && cb(null, tmp);
     }
   });
 }
